d4fac31cd23e68239f92dbb670ad0a8ce5c3b58f
Hai Nguyen <chaoticbob@me.com>
2017-08-07 09:14:29 -0700

    WIP

---
+++ structs/data/901_nested_structs.hlsl
@@ -0,0 +1,45 @@
+/* See build results at bottom of file */
+
+struct Outer {
+  struct Inner {
+    float4  InnerScale;
+  };
+
+  Inner   Nested;
+  float4  OuterScale;
+};
+
+struct VSOutput {
+  float4  Position : SV_POSITION;
+  float2  TexCoord  : TEXCOORD;
+};
+
+VSOutput VSMain(float4 Position : POSITION, float2 TexCoord : TEXCOORD)
+{
+  VSOutput ret;
+  ret.Position = Position;
+  ret.TexCoord = TexCoord;
+  return ret;
+}
+
+float4 PSMain(VSOutput input)
+{
+  float4 ret = float4(input.TexCoord, 0, 1);
+  return ret;
+}
+
+/* Build Results */
+/*
+
+FXC: FAILED
+  901_nested_structs.hlsl(4,10-14): error X3000: syntax error: unexpected token 'Inner'
+
+DXC:
+
+GLSLANG: FAILED
+  ERROR: 901_nested_structs.hlsl:6: 'member name' : Expected
+  ERROR: 901_nested_structs.hlsl:6: 'struct member declarations' : Expected
+  901_nested_structs.hlsl(6): error at column 4, HLSL parsing failed.
+  ERROR: 3 compilation errors.  No code generated.
+
+*/
